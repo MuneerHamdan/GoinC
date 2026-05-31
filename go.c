33caa357cda9892453019ce6639df7ad680e6ea0
MuneerHamdan <muneerhamdan01@gmail.com>
2026-05-31 13:30:17 -0400

    learning errno

--- go.c
+++ go.c
@@ -1,7 +1,13 @@
 #include <ncurses.h>
+#include <errno.h>
 
 int main(int argv, char** argc) {
-  WINDOW* window = initscr();
+  WINDOW* win;/* = initscr();*/
+  errno = leaveok(win, false);
+  errno = wrefresh(win);
+//  if (r == -1)
+  if (errno)
+    fprintf(stderr, "error refreshing%d\n", errno);
   endwin();
   return 0;
 }
